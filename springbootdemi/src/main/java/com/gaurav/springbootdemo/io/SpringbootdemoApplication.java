e24b40686d7d8dbca9fef804568993ba8311c15c
gauravghimiree <ghimiregaurav773@gmail.com>
2024-09-18 20:47:22 +0545

    here is a project setup for spring boot

---
+++ springbootdemi/src/main/java/com/gaurav/springbootdemo/io/SpringbootdemoApplication.java
@@ -0,0 +1,14 @@
+package com.gaurav.springbootdemo.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoApplication.class, args);
+
+		System.out.println("hi this is gaurav");
+	}
+}
